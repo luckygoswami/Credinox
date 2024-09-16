6083b81ec5e5bed9f94c1a30fbf65f81fbdc952d
Lucky Goswami <125822925+luckygoswami@users.noreply.github.com>
2024-09-16 08:14:29 +0530

    add session timeout

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,10 @@ import { collection, addDoc, getDocs } from "firebase/firestore";
 import CryptoJS from "crypto-js";
 
 import Dashboard from "./components/Dashboard";
-import AuthForm from './components/AuthForm'
+import AuthForm from "./components/AuthForm";
 import PasswordGenerator from "./components/PasswordGenerator";
+
+const SESSION_TIMEOUT = 60 * 60 * 1000; // 60 minutes in milliseconds
 
 function App() {
   const [user, setUser] = useState(null);
@@ -21,6 +23,44 @@ function App() {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
+  useEffect(() => {
+    const checkSession = () => {
+      const passmanLastLoginTime = localStorage.getItem("passmanLastLoginTime");
+      if (passmanLastLoginTime && Date.now() - Number(passmanLastLoginTime) > SESSION_TIMEOUT) {
+        handleLogout();
+      }
+    };
+
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        checkSession();
+        setUser(currentUser);
+        localStorage.setItem("passmanLastLoginTime", Date.now().toString());
+      } else {
+        setUser(null);
+        localStorage.removeItem("passmanLastLoginTime");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      fetchPasswords();
+      const interval = setInterval(() => {
+        if (
+          Date.now() - Number(localStorage.getItem("passmanLastLoginTime")) >
+          SESSION_TIMEOUT
+        ) {
+          handleLogout();
+        }
+      }, 60 * 1000); // Check every 1 minute
+
+      return () => clearInterval(interval);
+    }
+  }, [user]);
+
   const handleSignUp = async () => {
     try {
       await createUserWithEmailAndPassword(auth, userEmail, userPassword);
@@ -29,24 +69,16 @@ function App() {
     }
   };
 
-  // const handleSignIn = async () => {
-  //   try {
-  //     await signInWithEmailAndPassword(auth, userEmail, userPassword);
-  //   } catch (error) {
-  //     console.error("Error signing in", error);
-  //   }
-  // };
-
   const handleSignIn = () => {
     signInWithEmailAndPassword(auth, userEmail, userPassword)
       .then(() => {
         console.log("Sign in successful");
+        localStorage.setItem("passmanLastLoginTime", Date.now().toString());
       })
       .catch((error) => {
         console.error("Error signing in", error);
       });
   };
-
 
   const encryptPassword = (password) => {
     return CryptoJS.AES.encrypt(password, "secret-key").toString();
@@ -60,6 +92,7 @@ function App() {
   const handleLogout = async () => {
     setPasswords([]);
     await signOut(auth);
+    localStorage.removeItem("passmanLastLoginTime");
   };
 
   const savePassword = async () => {
@@ -87,13 +120,6 @@ function App() {
     });
     setPasswords(passwordList);
   };
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-      if (currentUser) fetchPasswords();
-    });
-  }, [user]);
 
   const DashboardProps = {
     user,
@@ -126,18 +152,21 @@ function App() {
       </header>
 
       <main className="container mx-auto my-10 flex justify-between px-10 overflow-auto">
-        {user ? (
-          <Dashboard {...DashboardProps} />
-        ) : (
-          <AuthForm {...AuthProps} />
-        )}
+        {user ? <Dashboard {...DashboardProps} /> : <AuthForm {...AuthProps} />}
         <PasswordGenerator />
       </main>
 
       <footer className="bg-gray-800 py-4">
         <div className="container mx-auto text-center text-gray-400">
           &copy; {new Date().getFullYear()} Passman. All rights reserved. <br />
-          Powered by <a href="https:github.com/Luckygoswami" target="_blank">Lucky Goswami</a>
+          Powered by{" "}
+          <a
+            href="https://github.com/Luckygoswami"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lucky Goswami
+          </a>
         </div>
       </footer>
     </div>
